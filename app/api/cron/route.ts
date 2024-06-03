44ed19ab69dfeec7d8104caeca21b67f6fe9aa63
RJGROUP01 <newanonymous595@gmail.com>
2024-06-04 03:04:10 +0530

    fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -7,7 +7,7 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import Product from "@/lib/model/product.model";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 200; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
